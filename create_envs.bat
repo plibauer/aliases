2d6139dbbe50f5454369c6f0c17fb9519dbe4c16
Paul Libauer <plibauer@gmail.com>
2019-08-06 15:41:54 +1000

    Update config for new trunk branch

--- create_envs.bat
+++ create_envs.bat
@@ -2,4 +2,4 @@ SET pyexe=C:\Python\python.exe
 SET pyscript=C:/cygwin/home/plibauer/aliases/make_env.py
 SET config=C:/cygwin/home/plibauer/aliases/make_env.config
 
-FOR %%A IN (83 90 91 92 93 94) DO ( %pyexe% %pyscript% --config %config% %%A > %%A"environment") 
+FOR %%A IN (83 90 91 92 93 94 95) DO ( %pyexe% %pyscript% --config %config% %%A > %%A"environment") 
